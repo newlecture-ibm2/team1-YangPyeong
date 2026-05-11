dba483c022433eeb12e9e9527ffb80657fc4dad7
skpark0205 <skpark0205@gmail.com>
2026-05-11 13:15:27 +0900

    chore: 불필요한 임시 테스트 스크립트 삭제

--- test_balance.py
+++
@@ -1,8 +0,0 @@
-import sys
-from sqlalchemy import create_engine, text
-
-engine = create_engine("postgresql+psycopg://farm:farmbalance1234!@localhost:5151/farm_db")
-with engine.connect() as conn:
-    print("balance_data count:", conn.execute(text("SELECT COUNT(*) FROM balance_data")).scalar())
-    for row in conn.execute(text("SELECT * FROM balance_data LIMIT 5")):
-        print(row)
